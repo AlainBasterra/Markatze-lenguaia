8c8ffc0cf979eb8add316be7cdb211d245bd5a12
AlainBasterra <basterra.alain@uni.eus>
2022-05-31 00:31:36 +0200

    yoksetio

---
+++ JQ/form_post.php
@@ -0,0 +1,13 @@
+<?php
+$izena = $_POST["izena"];
+$adina = $_POST["adina"];
+$kontaktuMota = $_POST["kontaktuMota"];
+
+echo $izena;
+echo gettype($izena);
+echo "<br>";
+echo $adina;
+echo gettype($adina);
+echo "<br>";
+echo $kontaktuMota;
+echo gettype($kontaktuMota);
